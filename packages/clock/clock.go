3245acc3d27bde18ce898ee7d80230f3fc4ccc6f
Chris Trotman <chris@trotman.io>
2015-09-23 21:01:37 +1000

    Remove half baked MultiClock

--- packages/clock/clock.go
+++ packages/clock/clock.go
@@ -17,14 +17,6 @@ type multiClock struct {
 	multiProducer i3.MultiProducer
 }
 
-//func MultiClock(format string, times map[string]time.Timezone) Generator {
-//	clocks := make(map[string]Clock)
-//	for abbr, zone := range times {
-//	}
-//}
-
-//func (m *Multi
-
 // Generate implements i3.Generator
 func (c Clock) Generate() ([]i3.Output, error) {
 	if c.Location == "" {
