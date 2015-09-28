fe169cfb85acdccdbd99915e5008f3cf39bd9f75
denbeigh <denbeigh@denbeighstevens.com>
2015-09-28 22:57:48 +1000

    Implemented WLAN device functionality

--- packages/network/wlan.go
+++ packages/network/wlan.go
@@ -2,16 +2,93 @@ package network
 
 import (
 	i3 "github.com/denbeigh2000/goi3bar"
+
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+)
+
+const (
+	essidRxStr    = "ESSID:\"(.*)\""
+	strengthRxStr = "Link Quality=(\\d+)/(\\d+)"
+)
+
+var (
+	essidRx    = regexp.MustCompile(essidRxStr)
+	strengthRx = regexp.MustCompile(strengthRxStr)
 )
 
 type WLANDevice struct {
-	NetworkDevice
+	BasicNetworkDevice
 
+	WarnThreshold int
+	CritThreshold int
+
+	// Signal strength as a number between 0 and 100
 	strength int
 	essid    string
 }
 
+func (d *WLANDevice) updateESSID(input string) error {
+	matches := essidRx.FindStringSubmatch(input)
+	if len(matches) < 2 {
+		return fmt.Errorf("Couldn't match ESSID")
+	}
+
+	d.essid = matches[1]
+
+	return nil
+}
+
+func (d *WLANDevice) updateStrength(input string) error {
+	matches := strengthRx.FindStringSubmatch(input)
+	if len(matches) < 3 {
+		return fmt.Errorf("Couldn't match strength")
+	}
+
+	strengthNum, errN := strconv.Atoi(matches[1])
+	strengthDenom, errD := strconv.Atoi(matches[1])
+	if errN != nil {
+		return errN
+	}
+	if errD != nil {
+		return errD
+	}
+
+	d.strength = (strengthNum * 100) / strengthDenom
+
+	return nil
+}
+
+func (d *WLANDevice) fetch() (string, error) {
+	output, err := exec.Command("iwconfig", d.Identifier).Output()
+	if err != nil {
+		return "", err
+	}
+	outputS := string(output)
+
+	return outputS, nil
+}
+
 func (d *WLANDevice) update() error {
+	d.BasicNetworkDevice.Update()
+
+	iwOut, err := d.fetch()
+	if err != nil {
+		return err
+	}
+
+	err = d.updateStrength(iwOut)
+	if err != nil {
+		return err
+	}
+
+	err = d.updateESSID(iwOut)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -22,5 +99,23 @@ func (d *WLANDevice) Generate() ([]i3.Output, error) {
 		return nil, err
 	}
 
-	return nil, nil
+	txt := fmt.Sprintf("%v: %v %v%% (%v)", d.Name, d.essid,
+		d.strength, d.ip)
+
+	var color string
+	switch {
+	case d.strength < d.CritThreshold:
+		color = "#FF0000"
+	case d.strength < d.WarnThreshold:
+		color = "#FFA500"
+	default:
+		color = "#00FF00"
+	}
+
+	out := i3.Output{
+		FullText: txt,
+		Color:    color,
+	}
+
+	return []i3.Output{out}, nil
 }
