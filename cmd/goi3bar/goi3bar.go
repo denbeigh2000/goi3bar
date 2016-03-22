ffcfe349056e29b3eb2f34e86964cc01d6f8476e
denbeigh <denbeigh@denbeighstevens.com>
2016-03-21 21:30:56 -0700

    Added sample ethernet config model

--- cmd/goi3bar/goi3bar.go
+++ cmd/goi3bar/goi3bar.go
@@ -5,6 +5,7 @@ import (
 	"github.com/denbeigh2000/goi3bar/packages/battery"
 	"github.com/denbeigh2000/goi3bar/packages/clock"
 	"github.com/denbeigh2000/goi3bar/packages/memory"
+	"github.com/denbeigh2000/goi3bar/packages/network"
 )
 
 // TODO: Flags
@@ -19,6 +20,7 @@ func main() {
 	cs.Register("memory", memory.Build)
 	cs.Register("clock", clock.Build)
 	cs.Register("battery", battery.Build)
+	cs.Register("network", network.Build)
 
 	bar, err := cs.Build()
 	if err != nil {
