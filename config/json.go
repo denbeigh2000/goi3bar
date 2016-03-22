801f9f46126bbe234216924af9b486996e029774
denbeigh <denbeigh@denbeighstevens.com>
2016-03-21 21:02:38 -0700

    Faced sad reality of JSON parsing

---
+++ config/json.go
@@ -0,0 +1,18 @@
+package config
+
+import "encoding/json"
+
+// This feels so wrong and it really oughtn't to exist. However, this really
+// appears to be the commonly accepted way to "defer" decoding parts of a JSON
+// blob, so to speak. I have tried all forms of modification and casting, have
+// looked into fatih's excellent structs package, tried recursively descending
+// the tree by casting it to a map[string]interface{} and encoding as I go, but
+// this is the way it has to be.
+func jsonReparse(i, o interface{}) (err error) {
+	marshalled, err := json.Marshal(i)
+	if err == nil {
+		err = json.Unmarshal(marshalled, o)
+	}
+
+	return
+}
