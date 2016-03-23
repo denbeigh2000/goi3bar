1ae9896d142225a981722ea57d9ce934a480c2c8
denbeigh <denbeigh@denbeighstevens.com>
2016-03-22 19:23:23 -0700

    Vastly simplified the memory builder

--- packages/memory/membuilder.go
+++ packages/memory/membuilder.go
@@ -2,56 +2,17 @@ package memory
 
 import (
 	. "github.com/denbeigh2000/goi3bar"
+	"github.com/denbeigh2000/goi3bar/config"
 
-	"fmt"
 	"time"
 )
 
-func Build(options interface{}) (Producer, error) {
-	memMap, ok := options.(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("Could not format given data into memory config")
+func (m memoryBuilder) Build(c config.Config) (Producer, error) {
+	err := c.ParseConfig(&m)
+	if err != nil {
+		return nil, err
 	}
 
-	if _, ok := memMap["refresh"]; !ok {
-		return nil, fmt.Errorf("Missing refresh field")
-	}
-
-	mb := memoryBuilder{
-		Name:    memMap["name"].(string),
-		Refresh: memMap["refresh"].(string),
-	}
-
-	if warnThresholdRaw, ok := memMap["warnThreshold"]; ok {
-		warnThreshold, ok := warnThresholdRaw.(float64)
-		if !ok {
-			return nil, fmt.Errorf("Failed to parse given warnThreshold")
-		}
-
-		if warnThreshold > 100 || warnThreshold < 1 {
-			return nil, fmt.Errorf("Invalid value for warnThreshold: %.0f", warnThreshold)
-		}
-
-		mb.WarnThreshold = int(warnThreshold)
-	}
-
-	if critThresholdRaw, ok := memMap["critThreshold"]; ok {
-		critThreshold, ok := critThresholdRaw.(float64)
-		if !ok {
-			return nil, fmt.Errorf("Failed to parse given critThreshold")
-		}
-
-		if critThreshold > 100 || critThreshold < 1 {
-			return nil, fmt.Errorf("Invalid value for critThreshold: %.0f", critThreshold)
-		}
-
-		mb.CritThreshold = int(critThreshold)
-	}
-
-	return mb.Build()
-}
-
-func (m memoryBuilder) Build() (Producer, error) {
 	interval, err := time.ParseDuration(m.Refresh)
 	if err != nil {
 		return nil, err
