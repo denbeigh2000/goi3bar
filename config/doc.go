4c8cb756aa0f1510d353997f591407ac7c01ef60
denbeigh <denbeigh@denbeighstevens.com>
2016-03-26 18:41:09 -0700

    Did more boring stuff

--- config/doc.go
+++ config/doc.go
@@ -1,22 +1,39 @@
 /*
-	The config package contains the API needed to make your plugin configurable.
+The config package contains the API needed to make your plugin configurable.
 
-	To be used in the Config file, your plugin must implement the Builder
-	interface, where it will be provided a Config object, which allows the plugin
-	programmer to load JSON into an annotated struct of their choice, using
-	ConfigSet's ParseConfig() method. From there, you can configure your
-	Producer as you see fit with your user-provided configuration.
+Making your plugin available in the config file
 
-	I have followed an API similar to that of golang's own database/sql.
-	When writing your plugin, you will need to register yourself in an init()
-	method in your package, and then the ConfigSet object will be aware of your
-	plugin and will use it when given the corresponding key.
-	This has the pleasant benefit that the user can simply add your custom package
-	to the config parser mainfile as an anonymous import (as you would a sql
-	driver), compile, and provide a config file.
+To be used in the config file, your plugin must implement the Builder
+interface, where it will be provided a Config object, which allows the plugin
+programmer to load JSON into an annotated struct of their choice, using
+ConfigSet's ParseConfig() method. From there, you can configure your
+Producer as you see fit with your user-provided configuration.
 
-	If you write something you think is really nifty, you are of course welcome
-	to submit a pull request to me at the home repository and have it included in
-	the default distribution.
+I have followed an API similar to that of golang's own database/sql.
+When writing your plugin, you will need to register yourself in an init()
+method in your package, and then the ConfigSet object will be aware of your
+plugin and will use it when given the corresponding key.
+
+After registering, you need to include your package as an anonymous import in
+the main function which parses the config file - you can find it in cmd/goi3bar
+
+Builder
+
+Build() is the method that is required to implement the Builder interface.
+It is provided a Config object, which offers ParseConfig() to cast the options
+subtree of your plugin's JSON block to a struct a-la json.Unmarshal. The Config
+object also has the raw interface{} of the options subtree, allowing you to
+inspect it manually, so you can provide a flexible API without the overhead of
+remarshalling through trial-and-error.
+See the network builders for an example of this.
+
+I have defined an interface instead of a simple function typedef because there
+are some opportunities for code re-use. See the CPU builder for an example.
+
+Get involved
+
+If you write something you think is really nifty, you are of course welcome
+to submit a pull request to me for consideration to have it included in the
+default distribution.
 */
 package config
--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Denbeigh Stevens
+// Copyright 2015-2016 Denbeigh Stevens
 
 /*
 goi3bar is a package that is capable of generating output suitable for i3bar.
@@ -8,42 +8,46 @@ documentation, found at http://i3wm.org/docs/i3bar-protocol.html
 How to put data on the bar
 
 A single i3bar output struct is represented as an Output. Any applet wanting
-to display information on the bar must create output of type []Output.
+to display information on the bar must create output of type []Output. The two
+interfaces that produce []Ouput are Producer and Generator.
 
-That sounds hard
+Registering new plugins
 
-Two interfaces have been defined to make creating output easier: Generator and
-Producer.
-
-Generator
-
-A Generator is a simple output generator, where output can be created without
-pausing to collect data. Canonical examples are a clock, free disk space and
-free memory.  A generator must be able to generate output on demand. If you
-cannot form your output generation into a non-blocking function, use a Producer.
-
-Producer
-
-a Producer is similar to a generator, but is responsible for managing when it
-delivers its updates. A Producer is usually used over a generator when there is
-a need to specially manage how your collection function is run, so the timing
-control is contained within the Producer. A canonical example is the Disk IO
-applet, which needs to collect data for a given duration before it can give
-average throughput.
-
-To be registered on the bar, the applet needs to implement the Producer
-interface. Generators can easily be made into Producers by using the
-BaseProducer struct with a Generator, and giving it an interval. The
-BaseProducer will call Generate every interval, and push the resultant
-updates down the channel.
-
-Registering new applets
-
-To register a new applet on the bar, prodvide a unique key and Producer to
-i3bar.Register.
+To register a new plugins on the bar, prodvide a unique key and Producer to
+I3bar.Register.
 Remember to specify an order of applets before starting the bar. This can be
-done by providing all keys in the desired order as a slice to the i3bar.Order
+done by providing all keys in the desired order as a slice to the I3bar.Order
 function.
+
+Producer and Generator
+
+The Producer is the interface that must be implemented when registering a plugin
+with an I3bar. It has one method, Produce(), which returns a channel of
+[]Output. Whenever the I3bar produces new output, it will use the most recently
+received []Output it received from that Producer.
+
+It is recommended that you implement this interface only if you have a need to
+manage your own output scheduling - if you can generate output in a non-blocking
+way, implement the Generator method instead.
+
+Examples of required implementation of the Producer interface can be found in
+the disk IO or CPU percentage plugins.
+
+If you do not need to manage your own scheduling, you should implement the
+Generator interface instead. It has one method, Generate(), that is assumed to
+be non-blocking and will be called at regular intervals. To register a generator
+with an I3bar, wrap it in a BaseProducer, which will return a Producer with
+managed scheduling.
+
+Examples of the Generator+BaseProducer plugins are the clock, battery, disk usage
+and network packages
+
+Builder
+
+The Builder is the interface that must be implemented when making your plugin
+available to the user through the configuration file interface. Look in the
+config package docs for detailed info on how to provide an API through the
+config file.
 
 */
 package goi3bar
