782966ce4e5a4e4a49add3a83efae76c0a5e81bd
denbeigh <denbeigh@denbeighstevens.com>
2016-05-29 14:45:39 -0700

    Did some refactoring of CPU preparing for unit tests

--- packages/cpu/cpu.go
+++ packages/cpu/cpu.go
@@ -11,7 +11,8 @@ import (
 // Cpu is a small CPU load monitor. It scrapes /proc/loadavg to display your
 // average # waiting threads over 1, 5 and 15 minute averages.
 type Cpu struct {
-	Name string
+	Name string `json:"name"`
+
 	// If the CPU loads exceeds these thresholds, they will be rendered in the
 	// corresponding state.
 	WarnThreshold float64 `json:"warn_threshold"`
@@ -27,7 +28,7 @@ const (
 )
 
 func (c *Cpu) populateValues() error {
-	load, err := load.Avg()
+	load, err := loadSource()
 	if err != nil {
 		return err
 	}
@@ -74,4 +75,27 @@ func (c *Cpu) Generate() ([]i3.Output, error) {
 	}
 
 	return outputs, nil
+}
+
+// Function that returns the CPU load average stat.
+// Used for unit testing purposes
+var loadSource loadFunc = defaultLoadFunc
+
+// Representation of CPU load average, used for unit testing.
+type loadInfo struct {
+	Load1  float64 `json:"load1"`
+	Load5  float64 `json:"load5"`
+	Load15 float64 `json:"load15"`
+}
+
+// Function used by the package to source CPU load average.
+type loadFunc func() (loadInfo, error)
+
+func defaultLoadFunc() (loadInfo, error) {
+	avg, err := load.Avg()
+	return loadInfo{
+		Load1:  avg.Load1,
+		Load5:  avg.Load5,
+		Load15: avg.Load15,
+	}, err
 }
